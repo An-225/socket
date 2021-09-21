045c9a84fad17a194e6069e3d75249e3ffa397a7
An-225 <66622436+An-225@users.noreply.github.com>
2021-09-21 08:53:35 -0300

    Funcionando sem classes

--- chat/chat_client.py
+++ chat/chat_client.py
@@ -1,48 +1,32 @@
 import socket
 
+ip = "192.168.0.105"
+port = 41990
 
-class Client:
-    __ip = str()
-    __port = int()
-    __bufferIN = str()
-    __bufferOUT = str()
+# Tipo de endereçamento (hostname ou IPV4 neste caso)
+# Tipo do socket (TCP neste caso)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def __init__(self, IP, PORT) -> None:
-        self.__ip = IP
-        self.__port = PORT
-        self.__conn = socket.socket(
-            # Tipo de endereçamento (hostname ou IPV4 neste caso)
-            socket.AF_INET,
-            socket.SOCK_STREAM  # Tipo do socket (TCP neste caso)
-        )
-        self.__conn.connect((self.__ip, self.__port))
+print(f"Conectando em {ip}:{port}...")
+client.connect((ip, port))
+print("Conectado!")
 
-    def __send(self):
-        self.__conn.send(self.__bufferOUT.encode("UTF-8"))
+while True:
+    # Mandando
+    bufferOUT = input("Digite sua mensagem:\n")
+    if bufferOUT == "quit":
+        client.send(bufferOUT.encode("UTF-8"))
+        print("Fechando conexao!")
+        client.close()
+        break
+    else:
+        client.send(bufferOUT.encode("UTF-8"))
 
-    def __recv(self):
-        self.__bufferIN = self.__conn.recvfrom(1024)
-
-    def __end(self):
-        self.__conn.close()
-
-    def mandar(self):
-        self.__bufferOUT = input("Digite sua mensagem:\n")
-        if self.__bufferOUT == "quit":
-            self.__end()
-        else:
-            self.__send()
-
-    def receber(self):
-        self.__recv()
-        print("Mensagem recebida:\n", self.__bufferIN)
-
-
-def main():
-    cliente = Client("192.168.0.105",41990)
-
-    while True:
-        cliente.mandar()
-        cliente.receber()
-
-main()
+    # Recebendo
+    print("Esperando mensagem...")
+    bufferIN = client.recvfrom(1024)
+    if bufferIN[0].decode("UTF-8") == "quit":
+        print("Servidor fechou conexao!!")
+        break
+    else:
+        print("Mensagem recebida:\n", bufferIN[0].decode("UTF-8"))
--- chat/chat_server.py
+++ chat/chat_server.py
@@ -1,52 +1,37 @@
 import socket
 
+ip = "192.168.0.105"
+port = 41990
 
-class Server:
-    __ip = str()
-    __port = int()
-    __bufferIN = str()
-    __bufferOUT = str()
+# Tipo de endereçamento (hostname ou IPV4 neste caso)
+# Tipo do socket (TCP neste caso)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((ip, port))
+server.listen(1)
 
-    def __init__(self, IP, PORT) -> None:
-        self.__ip = IP
-        self.__port = PORT
-        self.__conn = socket.socket(
-            # Tipo de endereçamento (hostname ou IPV4 neste caso)
-            socket.AF_INET,
-            socket.SOCK_STREAM  # Tipo do socket (TCP neste caso)
-        )
-        self.__conn.bind((self.__ip, self.__port))
-        self.__conn.listen(1)
-        self.__conn.accept()
+print(f"Servidor disponivel em {ip}:{port}")
+conn, source = server.accept()
+print(f"Conectado em: {source}")
 
+while True:
+    # Recebendo
+    print("Esperando mensagem...")
+    bufferIN = conn.recv(1024)
+    if bufferIN.decode("UTF-8") == "quit":
+        print("Cliente encerrou a conexao!")
+        print("Aguardando um novo cliente...")
+        conn, source = server.accept()
+        print(f"Conectado em: {source}")
+    else:
+        print("Mensagem recebida:\n", bufferIN.decode("UTF-8"))
 
-    def __send(self):
-        self.__conn.send(self.__bufferOUT.encode("UTF-8"))
-
-    def __recv(self):
-        self.__bufferIN = self.__conn.recv(1024)
-        self.__bufferOUT = self.__bufferIN.upper()
-        self.__send()
-
-    def __end(self):
-        self.__conn.close()
-
-    def mandar(self):
-        self.__bufferOUT = input("Digite sua mensagem:\n")
-        if self.__bufferOUT == "quit":
-            self.__end()
-        else:
-            self.__send()
-
-    def receber(self):
-        self.__recv()
-        print("Mensagem recebida:\n", self.__bufferIN)
-
-def main():
-    servidor = Server("192.168.0.105",41990)
-
-    while True:
-        servidor.receber()
-        #servidor.mandar()
-
-main()
+    # Mandando
+    bufferOUT = input("Digite sua mensagem:\n")
+    if bufferOUT == "quit":
+        print("Fechando conexao e servidor!")
+        conn.send(bufferOUT.encode("UTF-8"))
+        conn.close()
+        server.close()
+        break
+    else:
+        conn.send(bufferOUT.encode("UTF-8"))
