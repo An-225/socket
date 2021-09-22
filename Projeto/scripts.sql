fd022c569e656084a4147c9b5b31550418517ea2
An-225 <66622436+An-225@users.noreply.github.com>
2021-09-22 18:42:26 -0300

    SQL draft

---
+++ Projeto/scripts.sql
@@ -0,0 +1,18 @@
+//Criar
+CREATE TABLE IF NOT EXISTS notes(
+    note_id INTEGER PRIMARY KEY,
+    note_name TEXT NOT NULL,
+    note_text TEXT NOT NULL
+);
+//Ver
+SELECT * FROM notes;
+
+//Popular
+
+INSERT INTO notes (note_name,note_text) VALUES ('teste1','testando1');
+INSERT INTO notes (note_name,note_text) VALUES ('teste2','testando2');
+INSERT INTO notes (note_name,note_text) VALUES ('teste3','testando3');
+INSERT INTO notes (note_name,note_text) VALUES ('teste4','testando4');
+INSERT INTO notes (note_name,note_text) VALUES ('teste5','testando5');
+
+DROP TABLE notes;
