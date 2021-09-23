23f1184a05ade4863a5a7b3dfe42e30d7b07878a
An-225 <66622436+An-225@users.noreply.github.com>
2021-09-23 18:16:18 -0300

    newNote draft

--- Projeto/server.py
+++ Projeto/server.py
@@ -1,5 +1,6 @@
 import socket
 from database import Database
+from note import Note
 
 
 class Server:
@@ -10,9 +11,6 @@ class Server:
     bufferOUT = str()
 
     path = str()
-
-    # server obj
-    # conn
 
     def __init__(self, ip: str, port: int, path: str) -> None:
         self.ip = ip
@@ -28,11 +26,19 @@ class Server:
         self.connection, clientIP = self.server.accept()
 
     def receive(self):
-        self.bufferIN = (self.connection.recv(1024)).upper()
+        self.bufferIN = ((self.connection.recv(3)).decode("UTF-8")).upper()
 
         if "NEW" in self.bufferIN:
-            pass
+            self.newNote()
         if "ALL" in self.bufferIN:
             pass
         if "DEL" in self.bufferIN:
             pass
+
+    def newNote(self):
+        temp = Note()
+        
+        temp.name = self.connection.recv(128).decode("UTF-8")
+        temp.text = self.connection.recv(1024).decode("UTF-8")
+
+        self.data.saveNote(temp)
