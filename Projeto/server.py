e4528cbc04a0ae10339253c2a029f91a8fa96e11
An-225 <66622436+An-225@users.noreply.github.com>
2021-09-23 18:05:15 -0300

    open and receive drafts

--- Projeto/server.py
+++ Projeto/server.py
@@ -1,17 +1,38 @@
 import socket
 from database import Database
 
-class Server:
-    IP = str()
-    port = int()
-    #server obj
-    #conn
 
-    def __init__(self,ip:str, port:int,path:str) -> None:
-        data = Database(path)
+class Server:
+    ip = str()
+    port = int()
+
+    bufferIN = str()
+    bufferOUT = str()
+
+    path = str()
+
+    # server obj
+    # conn
+
+    def __init__(self, ip: str, port: int, path: str) -> None:
+        self.ip = ip
+        self.port = port
+        self.path = path
+
+    def open(self):
+        self.data = Database(self.path)
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((ip, port))
+        self.server.bind((self.ip, self.port))
         self.server.listen(1)
         self.connection, clientIP = self.server.accept()
 
+    def receive(self):
+        self.bufferIN = (self.connection.recv(1024)).upper()
+
+        if "NEW" in self.bufferIN:
+            pass
+        if "ALL" in self.bufferIN:
+            pass
+        if "DEL" in self.bufferIN:
+            pass
