ea8a9277d00e52e875d60834095e7ea0c78e696c
An-225 <66622436+An-225@users.noreply.github.com>
2021-09-23 20:48:28 -0300

    run draft

--- Projeto/server.py
+++ Projeto/server.py
@@ -30,19 +30,22 @@ class Server:
         self.server.close()
         del self.data
 
-    def receive(self) -> None:
-        self.bufferIN = ((self.connection.recv(3)).decode("UTF-8")).upper()
+    def receive(self,size) -> None:
+        self.bufferIN = ((self.connection.recv(size)).decode("UTF-8")).upper()
 
+    def send(self) -> None:
+        self.connection.send(self.bufferOUT.encode("UTF-8"))
+
+    def run(self) -> None:
+        self.receive(3)
+        
         if "NEW" in self.bufferIN:
             self.newNote()
         if "ALL" in self.bufferIN:
             self.allNotes()
         if "DEL" in self.bufferIN:
             self.delNote()
-
-    def send(self) -> None:
-        self.connection.send(self.bufferOUT.encode("UTF-8"))
-
+    
     def newNote(self) -> None:
         temp = Note()
         
