7d451725cbc88a126dc1cf8507bde96e1e5d5652
An-225 <66622436+An-225@users.noreply.github.com>
2021-09-23 21:20:13 -0300

    Debug prints

--- Projeto/database.py
+++ Projeto/database.py
@@ -4,9 +4,13 @@ from note import Note
 
 class Database:
     def __init__(self, path) -> None:
+        print(f"Connecting to database: {path}...",end='')
         self.__connection = sqlite3.connect(path)
         self.__cursor = self.__connection.cursor()
+        print(f"OK!")
 
+        
+        print("Initializing table...",end='')
         notesTable = """CREATE TABLE IF NOT EXISTS notes(
                         note_id INTEGER PRIMARY KEY,
                         note_name TEXT NOT NULL,
@@ -15,6 +19,7 @@ class Database:
 
         self.__cursor.execute(notesTable)
         self.__connection.commit()
+        print("OK")
     
     def __del__(self) -> None:
         self.__cursor.close()
@@ -22,27 +27,35 @@ class Database:
 
     def saveNote(self, note: Note) -> None:
         insert = f"INSERT INTO notes (note_name,note_text) VALUES ('{note.name}','{note.text}');"
-
+        
+        print("Saving note...",end='')
         self.__cursor.execute(insert)
         self.__connection.commit()
+        print("OK")
 
     def querryAll(self) -> list:
         querry = "SELECT * FROM notes;"
 
+        print("Querying notes...",end='')
         self.__cursor.execute(querry)
         data = self.__cursor.fetchall()
+        print("OK")
 
         Notes = list()
 
+        print("Creating list...",end='')
         for temp in data:
             tempNote = Note()
             tempNote.fromTuple(temp)
             Notes.append(tempNote)
+        print("OK")
         
         return Notes
 
     def deleteNote(self, note: Note) -> None:
         delete = f"DELETE FROM notes WHERE note_id IS {note.id};"
         
+        print("Deleting note...",end='')
         self.__cursor.execute(delete)
         self.__connection.commit()
+        print("OK")
